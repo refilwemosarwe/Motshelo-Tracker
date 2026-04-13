6fe9d8f04615126da657d72882c189a1b8a85ee0
JackJanet <janetlesokola@gmail.com>
2026-04-13 19:23:40 +0200

    Add contribution class with validation for amount

---
+++ contribution.java
@@ -0,0 +1,20 @@
+public class contribution {
+    private double amount;
+
+    public contribution(double amount) {
+        if (amount > 0) {
+            this.amount = amount;
+        } else {
+            System.out.println("Error: Contribution amount must be positive.");
+        }
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "contribution [amount=" + amount + "]";
+    }
+}
