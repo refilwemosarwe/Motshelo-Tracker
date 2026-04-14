cd114b70576ea73681c40a7a3043f63a900c00a2
Thabiso Tshepo <tshepoventures@gmail.com>
2026-04-14 10:49:21 +0200

    Update Contribution.java
    fixed indentation

--- src/com/ub/motshelocontributionstracker/services/Contribution.java
+++ src/com/ub/motshelocontributionstracker/services/Contribution.java
@@ -1,20 +1,26 @@
 package com.ub.motshelocontributionstracker.services;
-public class Contribution {
+
+public class Contribution
+{
     private double amount;
-
-    public Contribution(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Amount must be positive");
+    
+        public Contribution(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new IllegalArgumentException("Amount must be positive");
+            }
+                this.amount = amount;
         }
-        this.amount = amount;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
+    
+        public double getAmount()
+        {
+            return amount;
+        }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Contribution [amount=" + amount + "]";
     }
 }
