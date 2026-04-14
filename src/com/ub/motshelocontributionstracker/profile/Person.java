e5916084f6ed6afa5eeb194c087b164037e7f220
refilwemosarwe <mosarwerefilwe2@gmail.com>
2026-04-14 14:34:37 +0200

    Update Person.java

--- src/com/ub/motshelocontributionstracker/profile/Person.java
+++ src/com/ub/motshelocontributionstracker/profile/Person.java
@@ -1,1 +1,31 @@
 package com.ub.motshelocontributionstracker.profile;
+public class Person{
+    private String name;
+    private String phoneNumber;
+    private double totalContributions;
+
+    public Person(String name, String phoneNumber, double totalContributions){ 
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.totalContributions = 0.0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public double getTotalContributions(){
+        return totalContributions;
+    }
+
+    public void contribute(double amount){
+        if (amount > 0)
+        {
+            totalContributions += amount;
+        }
+    }
+}
