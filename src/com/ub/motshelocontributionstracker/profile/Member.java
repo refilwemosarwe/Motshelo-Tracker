cd79f652d3030d799ce33f11ee31c5561120723d
Thabiso Tshepo <tshepoventures@gmail.com>
2026-04-15 01:33:26 +0200

    Update Member.java
    added missing parentheses to the methods being called, removed method contribute() because it already exists in the parent class, added parameters to constructor

--- src/com/ub/motshelocontributionstracker/profile/Member.java
+++ src/com/ub/motshelocontributionstracker/profile/Member.java
@@ -5,23 +5,11 @@ public class Member extends Person
 {
     private int memberId;
 
-    public Member(int memberId)
+    public Member(int memberId, String name, String phoneNumber, totalContribution)
     {
         super(name, phoneNumber, totalContributions);
 
         this.memberId = memberId;
-    }
-    public void contribute(double amount)
-    {
-        if (amount > 0)
-        {
-            totalContributions += amount;
-        }
-    }
-
-    public double getTotalContributions()
-    {
-        return totalContributions;
     }
 
     public int getMemberId()
@@ -32,6 +20,6 @@ public class Member extends Person
     @Override
     public String toString()
     {
-        return "ID: " + getMemberId + " | Name: " + getName + " | Total: P" + getTotalContributions;
+        return "ID: " + getMemberId() + " | Name: " + getName() + " | Total: P" + getTotalContributions();
     }
 }
